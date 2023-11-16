4ed4483798ad28b96dcd42c4e811fc9e01d01c6b
albahu <97354572+albahu@users.noreply.github.com>
2023-11-16 16:10:16 -0700

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = https://63a5fab1-215b-4b49-bf84-da3e7d9f1c70.serverhub.tripleten-services.com
-CREATE_USER_PATH = /api/v1/users
-KITS_PATH = /api/v1/kits
+URL_SERVICE = 'https://63a5fab1-215b-4b49-bf84-da3e7d9f1c70.serverhub.tripleten-services.com'
+CREATE_USER_PATH = '/api/v1/users'
+KITS_PATH = '/api/v1/kits'
