55fa36466502fa77287c0b935616d4b0ea3141cf
albahu <97354572+albahu@users.noreply.github.com>
2023-11-22 20:12:31 -0700

    Update create_kit_name_kit_test.py

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,56 +1,60 @@
-from data import get_kit_body, send_post_request
+import sender_stand_request
+import pytest
+from data import KitBody
 
-def get_kit_body(name):
-    kit_body = {
-        "name": name,
+def run_test(test_name, kit_body, expected_status, expected_name=None):
+    response = sender_stand_request.send_post_request(kit_body)
 
-    }
-    return kit_body
+    assert response.status_code == expected_status
 
+    if expected_status == 201 and expected_name is not None:
+        assert response.json()["name"] == expected_name
 
-def get_new_user_token():
-
-    pass
-
-
-def test_allowed_characters_1():
+def test_positive_assert_1():
     # Escenario 1: El número permitido de caracteres (1)
-    kit_body = get_kit_body("a")
+    kit_body = KitBody.kit_body_1
+    run_test("Test 1", kit_body, 201, "a")
 
-def test_allowed_characters_2():
+def test_postive_assert_2():
     # Escenario 2: El número permitido de caracteres (511)
-    kit_body = get_kit_body('AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC')
+    kit_body = KitBody.kit_body_2
+    run_test('Test 2', kit_body, 201, 'AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC')
 
-def test_allowed_characters_3():
+def test_postive_assert_3():
     # Escenario 3: El número de caracteres es menor que la cantidad permitida (0)
-    kit_body = get_kit_body("")
+    kit_body = KitBody.kit_body_3
+    run_test('Test 3', kit_body, 400, '')
 
-def test_not_allowed_characters_4():
+def test_negative_assert_4():
     # Escenario 4: El número de caracteres es mayor que la cantidad permitida (512)
-    kit_body = get_kit_body('AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD')
+    kit_body = KitBody.kit_body_4
+    run_test('Test 4', kit_body, 400, 'AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD')
 
-def test_not_allowed_characters_5():
+def test_negative_assert_5():
     # Escenario 5: Se permiten caracteres especiales
-    kit_body = get_kit_body("*%@,")
+    kit_body = KitBody.kit_body_5
+    run_test('Test 5', kit_body, 201, '*%@,')
 
-def test_not_allowed_characters_6():
+def test_negative_assert_6():
     # Escenario 6: Se permiten espacios
-    kit_body = get_kit_body('A Aaa')
+    kit_body = KitBody.kit_body_6
+    run_test('Test 6', kit_body, 201, 'A Aaa')
 
-def test_not_allowed_characters_7():
+def test_negative_assert_7():
     # Esceneario 7: Se permiten números
-    kit_body = get_kit_body(123)
+    kit_body = KitBody.kit_body_7
+    run_test('Test 7', kit_body, 201, '123')
 
-def test_not_allowed_characters_8():
+def test_negative_assert_8():
     # Escenario 8: El parámetro no se pasa en la solicitud
-    kit_body = get_kit_body(None)
+    kit_body = KitBody.kit_body_8(None)
+    run_test('Test 8', kit_body, 400, '{}')
 
-def test_not_allowed_characters_9():
+def test_negative_assert_9():
     # Escenario 9: Se ha pasado un tipo de parámetro diferente (número)
-    kit_body = get_kit_body(123)
-
+    kit_body = KitBody.kit_body_9
+    run_test('Test 9', kit_body, 400, '123')
 
     if __name__ == '__main__':
         print('pass')
-
 
